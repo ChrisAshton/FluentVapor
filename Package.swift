221878eb96ca39a998f443d6719f9c251c957de7
Christopher Ashton <ashtoncr@gmail.com>
2018-07-03 19:35:32 -0400

    update packages

--- Package.swift
+++ Package.swift
@@ -5,16 +5,16 @@ let package = Package(
     name: "FluentVapor", // changed
     dependencies: [
         // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.5"),
 
         // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
-        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0-rc.2"),
+        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0-rc.4"),
 
         // Leaf package
-        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0-rc.2")  // added
+        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0-rc.2"),
     ],
     targets: [
-        .target(name: "App", dependencies: ["FluentSQLite", "Vapor", "Leaf"]), // "Leaf added"
+        .target(name: "App", dependencies: ["FluentSQLite", "Vapor", "Leaf"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
